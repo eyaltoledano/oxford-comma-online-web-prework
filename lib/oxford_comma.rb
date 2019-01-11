5061ab5ca68aedb919d8ac74a7ef2c77178f9a5f
Eyal Toledano <eyaltoledano@me.com>
2019-01-11 04:39:08 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,11 @@
 def oxford_comma(array)
+  if array.size == 1
+    array.first
+  elsif array.size == 2
+    array.join(" and ")
+  elsif array.size == 3
+    part1 = array.slice(0,2).join(", ")
+  else array.size > 3
+  end
 
 end
