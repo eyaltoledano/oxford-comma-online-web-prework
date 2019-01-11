cab67a708a99a6375de546ad525a5b36070d10ef
Eyal Toledano <eyaltoledano@me.com>
2019-01-11 05:04:02 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -9,7 +9,7 @@ def oxford_comma(array)
     full = "#{part1}, and #{part2}"
   else array.size > 3
     part2 = array[-1]
-    part1_prep = array.pop
+    array.pop
     part_1 = part1_prep.join(", ")
     full = "#{part1} and #{part2}"
   end
